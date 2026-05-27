f2cc916d3e61710fc20c6be62bdd6983357b946b
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-05-27 14:01:36 +0530

    updated

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -8,6 +8,11 @@ echo ${Fruits[0]}
 echo ${Fruits[@]}
 
 #How to run a command inside shell script
-
 Date=$(date +%F)
 echo "Today's date is $Date"
+
+#Adding two numbers
+Number1=$1
+Number2=$a
+Sum=$(($Number1+$Number2))
+echo "The total of $Number1 & $Number2 is $Sum"
