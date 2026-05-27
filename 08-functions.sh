c21802762f07597dacd6fc632c0fc2892b5dd20f
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-05-27 18:37:18 +0530

    Updated something in Shell Practice

--- 08-functions.sh
+++ 08-functions.sh
@@ -2,42 +2,31 @@
 
 #Functions
 
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then 
-    echo "Get the root access"
-    exit 1
-fi
 
 VALIDATE(){
+    USERID=$(id -u)
+
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Get the root access"
+        exit 1
+    else 
+        echo "Proceeding to install your requested Package $2"
+    fi
+        
     if [ $1 -ne 0 ]
-    then
+    then 
         echo "$2 is not installed, going to install"
         dnf install $3 -y
-        if [ $? -ne 0 ]
+        if [ $1 -ne 0 ]
         then
-            echo "$2 is not install, Please check the error"
+            echo "$2 is not install, check the error"
             exit 1
-        else 
-            echo "$2 is installed successfully"
+        else
+            echo "$2 installation is Successful"
         fi
     else
         echo "$2 is already installed, nothing to do"
-        # echo "$2 is aleady installed, I'm deleting it and reinstalling it"
-        # dnf remove $3 -y
-        # if [ $? -eq 0 ]
-        # then
-        #     echo "$2 is removed"
-        # fi
-        # dnf install $3 -y
-        # if [ $? -ne 0 ]
-        # then
-        #     echo "$2 is not install, check the error"
-        #     exit 1
-        # else
-        #     echo "$2 reinstalled Successfully"
-        # fi
     fi
 }
 
