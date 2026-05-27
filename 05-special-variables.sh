ec7f0ad2f342906fd02b8fd8119eebaa1513538b
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-05-27 14:39:50 +0530

    Updated something in Shell Practice

---
+++ 05-special-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Special variables
+echo "All the variables passed to the script (To declare the vaiables): $@"
+echo "Number of variables passed: $#"
+echo "Script file name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of current executing script: $$"
+sleep 10 &
+echo "PID of last background executing command: $!"
