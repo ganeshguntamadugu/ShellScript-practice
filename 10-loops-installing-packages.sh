0cb91eed597d9031ae256d34e4d49f1a89e8a864
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-05-31 17:39:19 +0530

    Updated something in Shell Practice

---
+++ 10-loops-installing-packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Color 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#Functions
+CLARITY(){
+    USERID=$(id -u)
+
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Get the root access"
+        exit 1
+    else 
+        echo "Proceeding to install your requested Package $1"
+    fi
+}
+
+VALIDATE(){    
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R NOT $N installed, going to install"
+        dnf install $2 -y
+        if [ $? -ne 0 ]
+        then
+            echo "$2 is not install, check the error"
+            exit 1
+        else
+            echo -e "$2 installation is $G Successful $N"
+        fi
+    else
+        echo -e "$2 is $G Already $N installed, nothing to do"
+    fi
+}
+
+for package in $@ 
+do 
+    CLARITY $@
+    dnf list installed $@
+    VALIDATE $? $@
+done
