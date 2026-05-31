75c3a51c5c21e807b144ae9202208c2930b0b2a1
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-05-31 17:18:11 +0530

    Updated something in Shell Practice

--- 09-loops.sh
+++ 09-loops.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#This is very tough
 echo "1"
 echo "2"
 echo "3"
@@ -10,3 +11,16 @@ echo "7"
 echo "8"
 echo "9"
 echo "10"
+
+#Using Loops to print numbers
+
+for i in 1,2,3,4,5,6,7,8,9
+do 
+    echo "$i"
+done
+
+#This is another simple way to use Loops
+for i in 1..100
+do
+    echo "$i"
+done
