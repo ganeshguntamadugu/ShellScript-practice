6314997805edafd03847d22b2898eb3446d15026
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-06-02 08:39:54 +0530

    Updated something in Shell Practice

--- 11-redirectors-logs.sh
+++ 11-redirectors-logs.sh
@@ -18,25 +18,27 @@ CLARITY(){
 
     if [ $USERID -ne 0 ]
     then 
-        echo "Get the root access" &>>$Log_file
+        echo "Get the root access"
         exit 1
+    else 
+        echo "Proceeding to install your requested Package $1"
     fi
 }
 
 VALIDATE(){    
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2 is$R NOT$N installed, going to install" &>>$Log_file
+        echo -e "$2 is$R NOT$N installed, going to install" 
         dnf install $2 -y
         if [ $? -ne 0 ]
         then
-            echo -e "$2 is $R not$N installed, check the error" &>>$Log_file
+            echo -e "$2 is $R not$N installed, check the error" 
             exit 1
         else
-            echo -e "$2 installation is$G Successful$N" &>>$Log_file
+            echo -e "$2 installation is$G Successful$N" 
         fi
     else
-        echo -e "$2 is$Y Already$N installed, nothing to do" &>>$Log_file
+        echo -e "$2 is$Y Already$N installed, nothing to do" 
     fi
 }
 
@@ -49,11 +51,10 @@ INPUT(){
 }
 
 INPUT
-CLARITY
 #Using loops
 for package in $@ 
-do
-    echo "Proceeding to install your requested Package $package" &>>$Log_file
-    dnf list installed $package &>>$Log_file
+do 
+    CLARITY $package
+    dnf list installed $package
     VALIDATE $? $package
 done
