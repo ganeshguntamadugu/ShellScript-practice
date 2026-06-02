f3e2c0018181fcd91f287aff69208f46ebb6cc17
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-06-02 08:07:00 +0530

    Updated something in Shell Practice

--- 09-loops.sh
+++ 09-loops.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 #This is very tough
-# echo "1"
-# echo "2"
-# echo "3"
-# echo "4"
-# echo "5"
-# echo "6"
-# echo "7"
-# echo "8"
-# echo "9"
-# echo "10"
+echo "1"
+echo "2"
+echo "3"
+echo "4"
+echo "5"
+echo "6"
+echo "7"
+echo "8"
+echo "9"
+echo "10"
 
-# echo ""
+echo ""
 
 # #Using Loops to print numbers
-# for i in 1 2 3 4 5 6 7 8 9
-# do 
-#     echo "$i"
-# done
+for i in 1 2 3 4 5 6 7 8 9
+do 
+    echo "$i"
+done
 
-# echo ""
+echo ""
 
 # # #This is another simple way to use Loops
-# for i in {1..100} #It means 1 to 100
-# do
-#     echo "$i"
-# done
+for i in {1..100} #It means 1 to 100
+do
+    echo "$i"
+done
 
-# echo ""
+echo ""
 
 if [ $# -eq 0 ]
 then
--- 11-redirectors-logs.sh
+++ 11-redirectors-logs.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
 
- 
+Log_folder=/var/log/shellscript-practice
+Script_name=$(echo $#) | awk -F "." '{print $F }'
+Log_file=$Log_folder $Script_name $(date).log
+
+mkdir $Log_folder
+
+ #Color 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#Functions
+CLARITY(){
+    USERID=$(id -u)
+
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Get the root access" &>>$Log_file
+        exit 1
+    else 
+        echo "Proceeding to install your requested Package $1" &>>$Log_file
+    fi
+}
+
+VALIDATE(){    
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is$R NOT$N installed, going to install" &>>$Log_file
+        dnf install $2 -y
+        if [ $? -ne 0 ]
+        then
+            echo -e "$2 is $R not$N installed, check the error" &>>$Log_file
+            exit 1
+        else
+            echo -e "$2 installation is$G Successful$N" &>>$Log_file
+        fi
+    else
+        echo -e "$2 is$Y Already$N installed, nothing to do" &>>$Log_file
+    fi
+}
+#Using loops
+
+for package in $@ 
+do 
+    CLARITY $package
+    dnf list installed $package &>>$Log_file
+    VALIDATE $? $package
+done
