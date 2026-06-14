1646755a2ab324af8f555a438d3a82e72fe1aa22
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-06-14 16:26:08 +0530

    Updated something in ShellScript-practice

---
+++ dummy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/ec2-user
+mkdir -p logs
+
+touch cat.log
+touch bat.java
+touch -d 20250702 rat.log
+touch -d 20250904 soop.log
+touch -d 20251001 cow.py
+touch -d 20260606 rat.log
+touch -d 20260524 goat.log
